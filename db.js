7ab4d1ff24348ab7902e6519207d1e1c6af177d1
Eduar Garcia <eduarfrancis11@gmail.com>
2025-07-16 15:20:51 -0600

    IMP - Sistema de facturación

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '111',
+    password: 'root',
     database: 'sistema_facturacion',
     connectionLimit: 10,
     waitForConnections: true,
